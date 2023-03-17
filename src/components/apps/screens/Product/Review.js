c1e9d16c94e652f86dbea7241838c3f998ab1c72
NguyenTrongHieu <ngochuongtr1975@gmail.com>
2023-03-17 16:37:51 +0700

    Vs4 - Hieu

--- src/components/apps/screens/Product/Review.js
+++ src/components/apps/screens/Product/Review.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, Image, TouchableOpacity, Dimensions, SafeAreaView, ScrollView } from 'react-native'
+import { StyleSheet, Text, View, Image, TouchableOpacity, Dimensions, ScrollView } from 'react-native'
 import React from 'react'
 
 const DataDemo = [
