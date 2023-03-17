531035d2b0d55986fdfc82d945b30d9f29facde8
NguyenTrongHieu <ngochuongtr1975@gmail.com>
2023-03-17 21:22:38 +0700

    Fix loi nho

--- src/components/apps/screens/Cart/CheckOut.js
+++ src/components/apps/screens/Cart/CheckOut.js
@@ -1,4 +1,4 @@
-import { Button, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 import { Entypo, AntDesign } from '@expo/vector-icons';
 
