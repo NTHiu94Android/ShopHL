bff76df717c5982b84f656a6632161b452c114e5
NguyenTrongHieu <ngochuongtr1975@gmail.com>
2023-03-18 16:27:12 +0700

    Copy code Nhu Anh

--- src/components/apps/screens/Product/ProductDetail.js
+++ src/components/apps/screens/Product/ProductDetail.js
@@ -1,18 +1,110 @@
-import { StyleSheet, Text, TouchableOpacity, View  } from 'react-native'
-import React from 'react';
-import back from '../../../backEvent/back';
+import { StyleSheet, Text, View, SafeAreaView, Image, Pressable, ScrollView, TouchableOpacity, FlatList } from 'react-native'
+import React, { useState } from 'react'
 
-const ProductDetail = (props) => {
-  const { navigation } = props;
-  back(navigation);
-  return (
-    <View style={{paddingTop: 50}}>
-      <TouchableOpacity onPress={()=>navigation.goBack()}><Text>Back</Text></TouchableOpacity>
-      <Text>ProductDetail</Text>
+const Item = ({ item, onPress }) => (
+  <TouchableOpacity onPress={onPress}>
+    <View style={{ flex: 1, flexDirection: 'column', padding: 4 }}>
+      <Image
+        style={{ flex: 1, width: 150, height: 96, borderRadius: 6, }}
+        resizeMode='cover'
+        source={{ uri: item.link, }} />
+      <View style={{ flex: 1 }}>
+        <Text
+          style={{ fontWeight: '400', fontSize: 13, lineHeight: 20, color: '#4e4b66' }}>
+          {item.field}
+        </Text>
+      </View>
     </View>
+  </TouchableOpacity>
+);
+const ProductDetail = ({ route }) => {
+  const { item } = route.params;
+  console.log("Detail item: ");
+  console.log(item);
+  return (
+    <View style={{ flex: 1 }}>
+      <View>
+        <Image
+          style={{ width: 400, height: 450, position: "relative", }}
+          resizeMode='cover'
+          source={{
+            uri: item.link,
+          }}
+        />
+        <View style={{ position: "absolute" }}>
+          <Image
+            resizeMode='cover'
+            source={require('../../../../assets/images/ic_back.png')}
+            style={{ width: 35, marginTop: 40, marginLeft: 27 }}
+          />
+          <Image
+            resizeMode='cover'
+            source={require('../../../../assets/images/ic_color.png')}
+            style={{ width: 35, height: 149, marginLeft: 27 }}
+          />
+        </View>
+      </View>
+      <View style={{ flex: 5, marginLeft: 24, marginTop: 15, marginRight: 24, }}>
+        <Text style={{ color: 'black', fontWeight: '800', fontSize: 24, marginTop: 16, lineHeight: 30.47 }}>{item.field}</Text>
+        <View style={{ flexDirection: 'row' }}>
+          <Text style={{ color: 'black', fontWeight: '700', fontSize: 30, lineHeight: 40.92, marginTop: 10, flex: 3 }}>
+            {item.price}
+          </Text>
+          <View style={{ flexDirection: 'row', margin: 20, flex: 1 }}>
+            <Image
+              style={{ width: 25, height: 25, marginRight: 15 }}
+              source={require('../../../../assets/images/btn_plus.png')}
+            />
+            <Text style={{ width: 25, height: 25, marginRight: 15 }}>
+              01
+            </Text>
+            <Image
+              style={{ width: 25, height: 25 }}
+              source={require('../../../../assets/images/btn_minus.png')}
+            />
+          </View>
+        </View>
+        <View style={{ flexDirection: 'row', marginTop: 10 }}>
+          <Image
+            style={{ width: 20, height: 20 }}
+            source={require('../../../../assets/images/ic_star.png')}
+          />
+          <Text style={{ color: 'black', fontWeight: '700', fontSize: 18, lineHeight: 24.55, marginLeft: 25 }}>
+            {item.reviews}
+          </Text>
+          <Text style={{ color: '#808080', fontWeight: '600', fontSize: 14, lineHeight: 24.55, marginLeft: 25 }}>
+            {item.status_review}
+          </Text>
+        </View>
+        <Text style={{ color: '#808080', fontWeight: '300', textAlign: 'justify', marginTop: 14 }}>
+          {item.description}
+        </Text>
+      </View>
+      {/* ---------------------- */}
+      <View style={{ flex: 1, flexDirection: 'row', margin: 25 }}>
+        <View style={{ marginVertical: 8, width: '20%', marginRight: 4 }}>
+          <TouchableOpacity>
+            <Image style={{ width: 24, height: 28, marginTop: 26, marginLeft: 18 }}
+              source={require('../../../../assets/images/ic_fvr.png')} />
+          </TouchableOpacity>
+        </View>
+        <View style={{ marginVertical: 8, width: '20%', marginRight: 4 }}>
+          <TouchableOpacity>
+            <TouchableOpacity>
+              <Image style={{ width: 280, height: 60, marginRigh: 20, marginTop: 18 }}
+                source={require('../../../../assets/images/btn_add.png')} />
+            </TouchableOpacity>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View >
   )
+
 }
+
 
 export default ProductDetail
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+
+})
--- src/components/apps/screens/TabScreen/Favorite.js
+++ src/components/apps/screens/TabScreen/Favorite.js
@@ -1,14 +1,131 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image, FlatList, TouchableOpacity } from 'react-native'
 import React from 'react'
 
 const Favorite = () => {
+
   return (
-    <View>
-      <Text>Favorite</Text>
+    <View style={{ position: 'relative', flex: 1 }}>
+      <View style={styles.header}>
+        <Image source={require('../../../../assets/images/Kinhlup.png')} style={styles.Icon} />
+        <Text style={styles.h1}>Favorites</Text>
+        <Image source={require('../../../../assets/images/Cart.png')} style={styles.Icon} />
+      </View>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Add all to my cart</Text>
+      </TouchableOpacity>
+      <FlatList
+        data={data}
+        renderItem={renderItem}
+        keyExtractor={item => item.id.toString()}
+      />
+
     </View>
+
   )
 }
 
 export default Favorite
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  header: {
+    margin: 20,
+    marginTop: "15%",
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  Icon: {
+    width: 24,
+    height: 24,
+  },
+  h1: {
+    fontWeight: "bold",
+    fontSize: 24,
+    flex: 8,
+    textAlign: 'center',
+  },
+  listItem: {
+    display: 'flex',
+    flexDirection: 'row',
+    marginVertical: 0,
+    padding: 10,
+    paddingVertical: 20,
+    marginHorizontal: 20,
+    borderTopWidth: 0.5,
+    borderRadius: 20,
+    borderColor: 'rgba(0, 0, 0, 0.2)',
+  },
+  imgLst: {
+
+  },
+  listItemName: {
+    flex: 5,
+    paddingStart: 20,
+  },
+  TextlstName: {
+    fontWeight: 'normal',
+    fontSize: 20,
+    marginBottom: 5,
+  },
+  TextlstPrice: {
+    fontWeight: 'bold',
+    fontSize: 20,
+    marginTop: 5,
+  },
+  listItemIcon: {
+    flex: 1,
+    justifyContent: 'space-between',
+  },
+  button: {
+    position: 'absolute',
+    width: '80%',
+    alignItems: 'center',
+    bottom: 20,
+    backgroundColor: '#242424',
+    borderRadius: 10,
+    paddingVertical: 15,
+    zIndex: 1,
+    alignSelf: 'center',
+
+  },
+  buttonText: {
+    color: '#FFFFFF',
+    fontSize: 20,
+    fontWeight: '400'
+  },
+
+})
+
+const renderItem = ({ item }) => {
+  return (
+    <View style={styles.listItem}>
+      <Image source={require('../../../../assets/images/Iphone14.png')} style={styles.imgLst} />
+      <View style={styles.listItemName}>
+        <Text style={styles.TextlstName}>{item.name}</Text>
+        <Text style={styles.TextlstPrice}>$ {item.price}</Text>
+      </View>
+      <View style={styles.listItemIcon}>
+        <View style={{ alignSelf: 'flex-start', width: '100%', alignItems: 'center' }}>
+          <Image source={require('../../../../assets/images/del.png')} style={{ width: 30, height: 30 }} />
+        </View>
+        <View style={{ alignSelf: 'flex-start', width: '100%', alignItems: 'center' }}>
+          <Image source={require('../../../../assets/images/shop.png')} style={{ width: 30, height: 30 }} />
+        </View>
+      </View>
+    </View>
+
+  );
+};
+
+
+const data = [
+  { id: 1, name: 'Iphone 14', price: 50.00, img: 'Iphone14.png' },
+  { id: 2, name: 'Iphone 13', price: 50.00, img: 'Iphone14.png' },
+  { id: 3, name: 'Iphone 12', price: 40.00, img: 'Iphone14.png' },
+  { id: 4, name: 'Iphone 11', price: 30.00, img: 'Iphone14.png' },
+  { id: 5, name: 'Iphone 10', price: 20.00, img: 'Iphone14.png' },
+  { id: 6, name: 'Iphone 9', price: 10.00, img: 'Iphone14.png' },
+  { id: 7, name: 'Iphone 8', price: 510.00, img: 'Iphone14.png' },
+  { id: 8, name: 'Iphone 7', price: 100.00, img: 'Iphone14.png' },
+  { id: 9, name: 'Iphone XS', price: 50.00, img: 'Iphone14.png' },
+]
